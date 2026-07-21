8a99599e6c060f1bbe5b947d0dbd1e83c0459658
poseyo-photobooth <alfashilla@gmail.com>
2026-07-21 23:41:53 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+const templateCards = document.querySelectorAll(".template-card");
 
+const previewImage = document.getElementById("previewImage");
+
+let selectedFrame = "assets/frames/frame1.png";
+
+templateCards.forEach(card => {
+
+card.addEventListener("click", () => {
+
+templateCards.forEach(item => {
+
+item.classList.remove("active");
+
+});
+
+card.classList.add("active");
+
+previewImage.src = card.dataset.preview;
+
+selectedFrame = card.dataset.frame;
+
+console.log("Frame dipilih:", selectedFrame);
+
+});
+
+});
