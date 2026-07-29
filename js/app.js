5c155af325817a733fe63eefe4ef24c191ff7fcc
poseyo-photobooth <alfashilla@gmail.com>
2026-07-29 23:58:43 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -25,3 +25,71 @@ const stripCtx = stripCanvas.getContext("2d");
 let cameraStream = null;
 let photos = [];
 let selectedFrame = "frame1";
+
+// ===============================
+// KONFIGURASI FRAME
+// ===============================
+
+const frameConfigs = {
+
+    frame1:{
+
+        overlay:"assets/frames/frame1_overlay.png",
+
+        canvasWidth:1181,
+        canvasHeight:1748,
+
+        photos:[
+
+            // FOTO 1
+            {
+                x:0,
+                y:0,
+                w:0,
+                h:0
+            }
+
+        ]
+
+    },
+
+    frame2:{
+
+        overlay:"assets/frames/frame2_overlay.png",
+
+        canvasWidth:1181,
+        canvasHeight:1748,
+
+        photos:[
+
+        ]
+
+    },
+
+    frame3:{
+
+        overlay:"assets/frames/frame3_overlay.png",
+
+        canvasWidth:1181,
+        canvasHeight:1748,
+
+        photos:[
+
+        ]
+
+    },
+
+    frame4:{
+
+        overlay:"assets/frames/frame4_overlay.png",
+
+        canvasWidth:1181,
+        canvasHeight:1748,
+
+        photos:[
+
+        ]
+
+    }
+
+};
